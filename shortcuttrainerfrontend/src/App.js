b239a2e76971c844c98c63b9742ba5eacc725540
MarlonResler <marlon.resler@gmail.com>
2023-11-06 20:49:07 +0100

    Startup React

---
+++ shortcuttrainerfrontend/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+}
+
+function MenuPoint(props) {
+  
+}
+
+function MyProfile(props) {
+  
+}
+
+function Landing(props) {
+  
+}
+
+function AllCourses(props) {
+  
+}
+
+function SignUp(props) {
+  
+}
+
+function Result(props) {
+  
+}
+
+function Test(props) {
+  
+}
+
+function Course(props) {
+  
+}
+
+function Learn(props) {
+  
+}
+
+export default App;
